0b2f67a3485cd5e94b4c19f2c6abc4d05fb0bd4c
utsavsahu12345 <utsavsahu12345@gmail.com>
2025-08-28 12:21:22 +0530

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -17,4 +16,5 @@ export default defineConfig({
       },
     }),
   ],
+  base: '/Fashion-Api-Store/',
 })
